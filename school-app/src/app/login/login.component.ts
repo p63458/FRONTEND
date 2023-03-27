f0cc3f11525894cf81f87eba112d6716e5f95e63
Divya Potharaj <potharajdivya@gmail.com>
2023-03-26 20:01:49 -0800

    initial frontend

---
+++ school-app/src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+   // message = "Hello Divya Welcome to LEARN TO AIM HIGH application";
+   
+  fontColor = 'blue';
+  sayHelloId = 1;
+  canClick = false;
+  message = 'Hello, World';
+
+  sayMessage() {
+    alert(this.message);
+  }
+
+}
