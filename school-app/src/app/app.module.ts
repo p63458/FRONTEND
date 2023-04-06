539d456c644160a994ee71cbba791372a83e4b66
Divya Potharaj <potharajdivya@gmail.com>
2023-04-06 15:44:28 -0800

    Binding the data from html to ts

--- school-app/src/app/app.module.ts
+++ school-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -19,7 +19,8 @@ import {MatInputModule} from '@angular/material/input';
     BrowserAnimationsModule,
     MatButtonModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
 
    /*  exports: [
--- school-app/src/app/login/login.component.ts
+++ school-app/src/app/login/login.component.ts
@@ -1,24 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  templateUrl: 'login.component.html',
+  styleUrls: ['login.component.css'],
 })
-export class LoginComponent {
-   // message = "Hello Divya Welcome to LEARN TO AIM HIGH application";
-   
-   username : string ="";
-   password : string ="";
-   show: boolean= false;
-   submit(){
-   console.log("user name is " + this.username)
-   this.clear();
-   }
-   clear(){
-   this.username ="";
-   this.password = "";
-   this.show = true;
-   }
+export class LoginComponent implements OnInit {
+   emailId :any ;
+  password :any;
+  title: any = 'Welcome to School App';
 
+  userForm: any;
+  studentLogin = new FormGroup({
+    emailId: new FormControl(''),
+    password : new FormControl('')
+  });
+  saveData(){
+    console.log("hello");
+    console.log(this.studentLogin.value,"-- consists of entire form data");
+  }
+  constructor() {
+    axios({
+      method: 'post',
+      url: '/login',
+      data: {
+        firstName: 'Finn',
+        lastName: 'Williams',
+      },
+    });
+  }
+  ngOnInit(): void {}
 }
