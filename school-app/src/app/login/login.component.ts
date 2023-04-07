6f4f584bdc214973c460de9b923c795976b49048
Divya Potharaj <potharajdivya@gmail.com>
2023-04-06 17:13:17 -0800

    Added binding variable fromhtml to ts using ngmodel and added bootstrap

--- school-app/src/app/login/login.component.ts
+++ school-app/src/app/login/login.component.ts
@@ -1,34 +1,51 @@
+//imports 
 import { Component, OnInit } from '@angular/core';
 import axios from 'axios';
 import { FormGroup, FormControl } from '@angular/forms';
 
+//Component decorator
 @Component({
   selector: 'app-login',
   templateUrl: 'login.component.html',
   styleUrls: ['login.component.css'],
 })
-export class LoginComponent implements OnInit {
-   emailId :any ;
-  password :any;
-  title: any = 'Welcome to School App';
 
+export class LoginComponent implements OnInit {
+  //variable declaration
+  emailId: any;
+  password: any;
   userForm: any;
+  //interpolation --> here we have text in title which is diplayed in the html file that is sharing data from ts --> html
+  title: any = 'Welcome to School App';
+ 
+  // this is to capture all the data from the html to ts
   studentLogin = new FormGroup({
     emailId: new FormControl(''),
-    password : new FormControl('')
+    password: new FormControl(''),
   });
-  saveData(){
-    console.log("hello");
-    console.log(this.studentLogin.value,"-- consists of entire form data");
+
+  saveData() {
+    console.log('Data captured from the html to ts file');
+    console.log(this.studentLogin.value, '-- consists of entire form data');
   }
+
+  //change in the text box the data is captured here from html to ts
+  check(value: any) {
+    this.emailId = value.target.value;
+    console.log(
+      this.emailId,
+      '--from onchange that is when ever there is the change in the text box the data is captured here'
+    );
+  }
+
   constructor() {
     axios({
       method: 'post',
       url: '/login',
       data: {
-        firstName: 'Finn',
-        lastName: 'Williams',
+       ...this.studentLogin.value
       },
+      
     });
   }
   ngOnInit(): void {}
